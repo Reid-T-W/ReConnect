08a9f5a667814d33bb74dad2cbf7ad419da1cde6
Reid-T-W <rediettadesse100@gmail.com>
2023-01-16 12:53:28 +0300

    to_search.js script commented

--- api/v1/static/scripts/to_search.js
+++ api/v1/static/scripts/to_search.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
   $("#search").click(function () {
+    // Redirect to search page when search button is clicked
     window.location.href = 'http://localhost:5000/search';
   });
 });
